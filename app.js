e9730d8f11296893c5c32051c9e09ed2388943fd
Abdelnasser2003 <abdelnass455@gmail.com>
2026-07-20 02:54:48 +0100

    Ajouter la logique du QCM
    .function verifier(reponse){
    
    if(reponse=="B"){
    
    document.getElementById("resultat").innerHTML="✅ Bonne réponse !";
    
    }else{
    
    document.getElementById("resultat").innerHTML="❌ Mauvaise réponse.<br>La bonne réponse est : BCG.";
    
    }
    
    }

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    alert("Bienvenue sur IDE Smart Cameroun !");
-});
+function verifier(reponse){
+
+if(reponse=="B"){
+
+document.getElementById("resultat").innerHTML="✅ Bonne réponse !";
+
+}else{
+
+document.getElementById("resultat").innerHTML="❌ Mauvaise réponse.<br>La bonne réponse est : BCG.";
+
+}
+
+}
