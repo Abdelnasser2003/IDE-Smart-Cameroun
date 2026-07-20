401c497fe7c73f5a18c308bd75f26bec4d9eb966
Abdelnasser2003 <abdelnass455@gmail.com>
2026-07-20 18:47:49 +0100

    Ajout du système de quiz
    let score = 0;
    
    let numero = 0;
    
    const questions = [
    
    {
    question:"Le vaccin contre la tuberculose est :",
    A:"VPO",
    B:"BCG",
    C:"VAR",
    D:"VAT",
    bonne:"B"
    },
    
    {
    question:"Le paludisme est transmis par :",
    A:"La mouche",
    B:"Le moustique Anopheles",
    C:"Le chien",
    D:"Le rat",
    bonne:"B"
    },
    
    {
    question:"La température normale est :",
    A:"35°C",
    B:"36°C",
    C:"37°C",
    D:"39°C",
    bonne:"C"
    }
    
    ];
    
    function afficherQuestion(){
    
    document.getElementById("question").innerHTML=questions[numero].question;
    
    let boutons=document.getElementsByTagName("button");
    
    boutons[0].innerHTML="A. "+questions[numero].A;
    
    boutons[1].innerHTML="B. "+questions[numero].B;
    
    boutons[2].innerHTML="C. "+questions[numero].C;
    
    boutons[3].innerHTML="D. "+questions[numero].D;
    
    }
    
    function verifier(rep){
    
    if(rep==questions[numero].bonne){
    
    score++;
    
    document.getElementById("resultat").innerHTML="✅ Bonne réponse";
    
    }else{
    
    document.getElementById("resultat").innerHTML="❌ Mauvaise réponse";
    
    }
    
    numero++;
    
    if(numero<questions.length){
    
    setTimeout(function(){
    
    document.getElementById("resultat").innerHTML="";
    
    afficherQuestion();
    
    },1000);
    
    }else{
    
    setTimeout(function(){
    
    document.body.innerHTML="<h1>🎉 Fin du Quiz</h1><h2>Score : "+score+" / "+questions.length+"</h2>";
    
    },1000);
    
    }
    
    }
    
    afficherQuestion();

--- app.js
+++ app.js
@@ -1,13 +1,90 @@
-function verifier(reponse){
+let score = 0;
 
-if(reponse=="B"){
+let numero = 0;
 
-document.getElementById("resultat").innerHTML="✅ Bonne réponse !";
+const questions = [
+
+{
+question:"Le vaccin contre la tuberculose est :",
+A:"VPO",
+B:"BCG",
+C:"VAR",
+D:"VAT",
+bonne:"B"
+},
+
+{
+question:"Le paludisme est transmis par :",
+A:"La mouche",
+B:"Le moustique Anopheles",
+C:"Le chien",
+D:"Le rat",
+bonne:"B"
+},
+
+{
+question:"La température normale est :",
+A:"35°C",
+B:"36°C",
+C:"37°C",
+D:"39°C",
+bonne:"C"
+}
+
+];
+
+function afficherQuestion(){
+
+document.getElementById("question").innerHTML=questions[numero].question;
+
+let boutons=document.getElementsByTagName("button");
+
+boutons[0].innerHTML="A. "+questions[numero].A;
+
+boutons[1].innerHTML="B. "+questions[numero].B;
+
+boutons[2].innerHTML="C. "+questions[numero].C;
+
+boutons[3].innerHTML="D. "+questions[numero].D;
+
+}
+
+function verifier(rep){
+
+if(rep==questions[numero].bonne){
+
+score++;
+
+document.getElementById("resultat").innerHTML="✅ Bonne réponse";
 
 }else{
 
-document.getElementById("resultat").innerHTML="❌ Mauvaise réponse.<br>La bonne réponse est : BCG.";
+document.getElementById("resultat").innerHTML="❌ Mauvaise réponse";
+
+}
+
+numero++;
+
+if(numero<questions.length){
+
+setTimeout(function(){
+
+document.getElementById("resultat").innerHTML="";
+
+afficherQuestion();
+
+},1000);
+
+}else{
+
+setTimeout(function(){
+
+document.body.innerHTML="<h1>🎉 Fin du Quiz</h1><h2>Score : "+score+" / "+questions.length+"</h2>";
+
+},1000);
 
 }
 
 }
+
+afficherQuestion();
